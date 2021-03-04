afee215538cc87e2614be06b4d8d92466a019d56
mikeAdamss <mikelovesbooks@gmail.com>
2021-03-04 13:13:06 +0000

    tidy up and fix

--- generate_results.sh
+++
@@ -1,5 +0,0 @@
-
-python3 ./download_from_bucket.py
-python3 ./build_dynamic_tests.py
-behave -f allure_behave.formatter:AllureFormatter -o allure-results ./features
-cp ./categories.json ./allure-results/categories.json
--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
-# Pull the previous results from the google bucket then generate new test reports
+# Pull the previous results from the google bucket
 # NOTE - we're nuking whats in the bucket after we copy it (to clean old kruft before we upload again later)
 # longer term, we probably want something a bit less drastic (or a backup at least).
-./generate_results.sh
+python3 ./download_from_bucket.py
+
+# generate new tests
+python3 ./build_dynamic_tests.py
+
+# run test, format output for allure
+behave -f allure_behave.formatter:AllureFormatter -o allure-results ./features
+
+# copy in our custom categories (in case we've changed them)
+cp ./categories.json ./allure-results/categories.json
 
 # Turn the new reports into test results
 docker run -v $PWD:/workspace -w /workspace frankescobar/allure-docker-service /bin/bash -c "allure generate allure-results --clean"
