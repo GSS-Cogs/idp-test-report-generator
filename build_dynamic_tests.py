7be098c4a324390be5a9070f959c469f7dc9033c
mikeAdamss <mikelovesbooks@gmail.com>
2021-03-08 16:19:24 +0000

    reasonably mvp

--- build_dynamic_tests.py
+++ build_dynamic_tests.py
@@ -3,6 +3,7 @@ import os
 from os import listdir
 import json
 import yaml
+from pathlib import Path
 
 from github import Github
 import requests
@@ -34,24 +35,33 @@ def info_as_dict(url):
     return info_dict
 
 
-def build_per_scraper_temp_scenario(info_json_path, dataset):
+def build_test_with_seed(info_json_path, dataset, is_temp=False):
     """
-    Create a scenario for a temp scraper
+    Create a scenario for using a scraper from a seed
     """
+    mod_text = "Temp " if is_temp else ""
+
+    # Test content
+    if is_temp:
+        contents_check = ""
+    else:
+        contents_check = "And the scraper contains at least one valid distribution"
     return f"""
-  Scenario: Temp Scraper for {dataset}
-    Given we specify the info json "{info_json_path}"
-    Then when we scrape no exception is encountered 
+  Scenario: {mod_text}Scraper for {dataset}
+    Given we use the seed "{info_json_path}"
+    Then when we scrape with the seed no exception is encountered
+    {contents_check}
     """
 
-def build_per_scraper_non_temp_scenario(landing_page, dataset):
+def build_test_with_url(url, dataset, is_temp=False):
     """
-    Given a scraper and a url, write the test scenario
+    Create a scenario for using a scraper from a seed
     """
+    mod_text = "Temp " if is_temp else ""
     return f"""
-  Scenario: Scraper for {dataset}
-    Given we specify the url "{landing_page}"
-    Then when we scrape no exception is encountered 
+  Scenario: {mod_text}Scraper for {dataset}
+    Given we specify the url "{url}"
+    Then when we scrape with thr url no exception is encountered 
     """
 
 g = git_if_needed()
@@ -60,6 +70,9 @@ g = git_if_needed()
 with open("./config.yaml") as f:
     config_dict = yaml.load(f, Loader=yaml.FullLoader)
     familes = config_dict["families"]
+
+seed_path = Path("out/seeds")
+seed_path.mkdir(exist_ok=True, parents=True)
 
 for family in familes:
 
@@ -91,36 +104,37 @@ for family in familes:
     contents = repo.get_contents("datasets")
     for content_file in contents:
         file_content = contents.pop(0)
+
+        if 'Jenkinsfile' in content_file.path:
+            continue
+            
         if file_content.type == "dir" and "." not in content_file.path and 'Makefile' not in content_file.path:
 
             info_url = f'https://raw.githubusercontent.com/{family}/master/{content_file.path}/info.json'
             info_dict = info_as_dict(info_url)
 
-            # TODO
-            if "dataURL" in info_dict.keys():
-                seed = "./info.json"
+            info_dict_path = f"./out/seeds/{content_file.path.split('/')[1]}_info.json"
                 
-                with open(seed, 'w') as f:
-                    json.dump(info_dict, f)
-
-                scenario = build_per_scraper_temp_scenario(info_dict, f'{content_file.path.split("/")[1]}')
-                with open(f"./features/{family.split('/')[1]}-temp-scrapers.feature", 'a') as f_temp:
-                    f_temp.write("\n")
-                    f_temp.write(scenario)
+            with open(info_dict_path, 'w') as f:
+                json.dump(info_dict, f, indent=2)
 
             # Multiple landingPage urls specified
-            elif isinstance(info_dict["landingPage"], list):
+            if isinstance(info_dict["landingPage"], list):
                 for i, url in enumerate(info_dict["landingPage"]):
-                    scenario = build_per_scraper_non_temp_scenario(url, f'{content_file.path.split("/")[1]}_{i}')
+                    scenario = build_test_with_url(url, f'{content_file.path.split("/")[1]}_{i}', is_temp="dataURL" in info_dict.keys())
                     with open(f"./features/{family.split('/')[1]}-multi-url-scrapers.feature", 'a') as f_multi:
                         f_multi.write("\n")
                         f_multi.write(scenario)
 
             # Single landing page specified
             else:
-                with open(f"./features/{family.split('/')[1]}-standard-scrapers.feature", 'a') as f_base:
-                    scenario = build_per_scraper_non_temp_scenario(info_dict["landingPage"], content_file.path.split('/')[1])
-                    f_base.write("\n")
-                    f_base.write(scenario)
-
-
+                if "dataURL" in info_dict.keys():
+                    with open(f"./features/{family.split('/')[1]}-temp-scrapers.feature", 'a') as f_temp:
+                        scenario = build_test_with_seed(info_dict_path, content_file.path.split('/')[1], is_temp=True)
+                        f_temp.write("\n")
+                        f_temp.write(scenario)
+                else:
+                    with open(f"./features/{family.split('/')[1]}-standard-scrapers.feature", 'a') as f_base:
+                        scenario = build_test_with_seed(info_dict_path, content_file.path.split('/')[1], is_temp=False)
+                        f_base.write("\n")
+                        f_base.write(scenario)
--- features/steps/base_scrapes.py
+++ features/steps/base_scrapes.py
@@ -1,15 +1,39 @@
 
+import json
+import logging
+
 from behave import *
 from allure_behave.hooks import allure_report
-
 from gssutils import Scraper
+
+from helpers import parse_scrape_to_json
 
 @given('we specify the url "{url}"')
 def step_impl(context, url):
     context.url = url
-    
-@then('when we scrape no exception is encountered')
+
+@given(u'we use the seed "{seed_path}"')
+def step_impl(context, seed_path):
+    context.seed_path = seed_path
+
+@then('when we scrape with the seed no exception is encountered')
 def step_impl(context):
-    Scraper(context.url)
+    context.scrape = Scraper(seed=context.seed_path)
+
+@then('when we scrape with thr url no exception is encountered')
+def step_impl(context):
+    context.scrape = Scraper(context.url)
+
+@then('the scraper contains at least one valid distribution')
+def step_impl(context):
+    err_msg = 'Content fault. The scrape does not appear to contain a distribution'
+    try:
+        try:
+            assert len(context.scrape.distributions) > 0, err_msg
+        except:
+            context.scrape.select_dataset(latest=True)
+            assert len(context.scrape.distributions) > 0, err_msg
+    except Exception as err:
+        raise Exception(err_msg +'\n' + json.dumps(parse_scrape_to_json(context.scrape), indent=2)) from err
 
 allure_report("./allure-report")
